33219ce21551c65ac88c685619756e503e3e24cd
hmz4dev <hmz4dev@gmail.com>
2026-03-16 13:18:54 +0100

    load SsoToken

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,8 @@ use DevOps213\SSOauthenticated\Http\Controllers\SsoLoginController;
 Route::middleware('web')->group(function () {
     Route::get('/sso/login', [SsoLoginController::class, 'login'])->name('sso.login');
     Route::get('/profile', [SsoLoginController::class, 'profile'])->name('profile');
-    Route::get('/authentication', [SsoLoginController::class, 'authentication'])
+    Route::get('/auth/authentication', [SsoLoginController::class, 'authentication'])
         ->name('auth.sso.authentication');
+
+
 });
